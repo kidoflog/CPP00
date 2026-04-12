08b25f0e881cbbafd94187280d621cc56a818261
kkido <kkido@student.42tokyo.jp>
2026-04-12 17:04:36 +0900

    ex00done

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cctype>
+
+int main(int argc, char *argv[])
+{
+
+	if (argc < 2)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 0;
+	}
+	for (int arg_i = 1; argv[arg_i]; arg_i++)
+	{
+		for (int i = 0; argv[arg_i][i]; i++)
+		{
+			std::cout << (char)std::toupper(argv[arg_i][i]);
+		}
+	}
+	std::cout << std::endl;
+	return 0;
+}
